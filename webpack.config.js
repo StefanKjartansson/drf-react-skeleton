0c8db4b4499393d1df7888a290fe8fc5e420ceea
Stefan Kjartansson <esteban.supreme@gmail.com>
2016-08-17 06:14:47 +0000

    Load from webpack directory

--- webpack.config.js
+++ webpack.config.js
@@ -1,38 +1,7 @@
-import path from 'path';
-import CompressionPlugin from 'compression-webpack-plugin';
-import {FRONTEND_DIR, PROJECT, DIST_PATH} from './config';
+import dev from './webpack/dev';
+import production from './webpack/production';
 
-module.exports = {
-  entry: `${FRONTEND_DIR}/app.js`,
-  output: {
-    path: path.join(__dirname, DIST_PATH),
-    publicPath: DIST_PATH,
-    filename: `${PROJECT}.js`
-  },
-  module: {
-    loaders: [
-    {
-      test: /\.json$/,
-      loader: 'json'
-    },
-    {
-      test: /\.js?$/,
-      exclude: /(node_modules|bower_components)/,
-      loader: 'babel',
-      query: {stage: 0},
-    }
-    ]
-  },
-  plugins: [
-    new CompressionPlugin({
-        asset: "{file}.gz",
-        algorithm: "gzip",
-        regExp: /\.js$|\.html$/,
-        threshold: 10240,
-        minRatio: 0.8
-    })
-  ],
-  node: {
-   "child_process": "empty"
- }
-};
+module.exports = dev;
+if (process.env === 'production') {
+  module.exports = production;
+}
