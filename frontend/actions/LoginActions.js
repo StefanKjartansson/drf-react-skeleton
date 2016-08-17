22a21560efca8767981e4d72395f97a3ec2f16ec
Stefan Kjartansson <esteban.supreme@gmail.com>
2016-08-17 20:48:05 +0000

    es6 updates

--- frontend/actions/LoginActions.js
+++ frontend/actions/LoginActions.js
@@ -1,27 +1,25 @@
-import AppDispatcher from '../dispatchers/AppDispatcher.js';
-import {LOGIN_USER, LOGOUT_USER} from '../constants/LoginConstants.js';
-import RouterContainer from '../services/RouterContainer';
+import AppDispatcher from '../dispatchers/AppDispatcher';
 
-export default {
-  loginUser: (jwt) => {
-    let savedJwt = localStorage.getItem('jwt');
-    if (savedJwt !== jwt) {
-      localStorage.setItem('jwt', jwt);
-    }
-    let nextPath = RouterContainer.get().getCurrentQuery().nextPath || '/';
-    RouterContainer.get().transitionTo(nextPath);
+import {
+  LOGIN_USER,
+  LOGOUT_USER,
+} from '../constants/LoginConstants';
 
-    AppDispatcher.dispatch({
-      actionType: LOGIN_USER,
-      jwt: jwt
-    });
-  },
-
-  logoutUser: () => {
-    RouterContainer.get().transitionTo('/login');
-    localStorage.removeItem('jwt');
-    AppDispatcher.dispatch({
-      actionType: LOGOUT_USER
-    });
+export function loginUser(response) {
+  const jwt = response.token;
+  const savedJwt = localStorage.getItem('jwt');
+  if (savedJwt !== jwt) {
+    localStorage.setItem('jwt', jwt);
   }
+  AppDispatcher.dispatch({
+    actionType: LOGIN_USER,
+    data:       response,
+  });
+};
+
+export function logoutUser() {
+  localStorage.removeItem('jwt');
+  AppDispatcher.dispatch({
+    actionType: LOGOUT_USER,
+  });
 };
