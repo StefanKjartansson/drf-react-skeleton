68fae06165e1e0e115b2291036455b4d06e750f4
Stefan Kjartansson <esteban.supreme@gmail.com>
2016-08-18 06:10:05 +0000

    Update url usage, add test

---
+++ project/tests/test_views.py
@@ -0,0 +1,5 @@
+
+
+def test_index(client):
+    response = client.get('/')
+    assert response.status_code == 200
--- project/urls.py
+++ project/urls.py
@@ -1,22 +1,8 @@
-"""
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/1.8/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  url(r'^$', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  url(r'^$', Home.as_view(), name='home')
-Including another URLconf
-    1. Add an import:  from blog import urls as blog_urls
-    2. Add a URL to urlpatterns:  url(r'^blog/', include(blog_urls))
-"""
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
 from rest_framework.routers import DefaultRouter
+from rest_framework_jwt import views as jwt_views
 
 router = DefaultRouter()
 
@@ -24,8 +10,8 @@ router = DefaultRouter()
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api/', include(router.urls)),
-    url(r'^api-token-auth/', 'rest_framework_jwt.views.obtain_jwt_token'),
-    url(r'^api-token-refresh/', 'rest_framework_jwt.views.refresh_jwt_token'),
-    url(r'^api-token-verify/', 'rest_framework_jwt.views.verify_jwt_token'),
+    url(r'^api-token-auth/', jwt_views.obtain_jwt_token),
+    url(r'^api-token-refresh/', jwt_views.refresh_jwt_token),
+    url(r'^api-token-verify/', jwt_views.verify_jwt_token),
     url(r'^$', TemplateView.as_view(template_name='index.html'))
 ]
