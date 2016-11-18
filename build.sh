434873c2d3e30229c4f64d7aaa9042384965bd6f
Stefan Kjartansson <esteban.supreme@gmail.com>
2016-11-18 08:48:48 +0000

    Update build process to have no npm caching step because yarn

--- build.sh
+++ build.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 
 set -e
-# set -x
 
 NAME=${PROJECT:-project}
 DIST_PATH=${DIST_PATH:-"$NAME"/static/"$NAME"}
@@ -9,49 +8,35 @@ OWNER_UID=$(id -u)
 BRANCH=$(git rev-parse --symbolic-full-name --abbrev-ref HEAD)
 IS_PRODUCTION=0
 if [ "$BRANCH" = "master" ]; then IS_PRODUCTION=1; fi
+script_name=$0
 
-. color_log.sh
+function log() {
+    function logline() {
+        colorprint $1 "[$script_name] $2: $3"
+    }
 
-function sum {
-    md5sum $1 | cut -d ' ' -f 1
+    case $1 in
+        INFO)
+            logline "1;37" "$1" "$2" ;;
+        WARN)
+            logline 33 "$1" "$2" >&2 ;;
+        ERROR)
+            logline 31 "$1" "$2" >&2 ;;
+        DEBUG)
+            logline 0 "$1" "$2" ;;
+        SUCCESS)
+            logline "32" "$1" "$2" ;;
+        *)
+            log "ERROR" "unknown log level"
+            return 1
+            ;;
+    esac
 }
 
-function has_changed {
-    mkdir -p .cache
-    local p="$PWD/.cache/$1"
-    if [ ! -f "$p" ]; then
-        echo "true"
-        return
-    fi
-    n=$(sum $1)
-    o=$(cat $p)
-    if [ "$n" == "$o" ]; then
-        echo "false"
-    else
-        echo "true"
-    fi
-}
-
-function npm_cache {
-    log "INFO" "Starting ${FUNCNAME[0]}"
-    local cache_image="$NAME"-npm
-    local cache_dir=node_modules
-    local cache_file=package.json
-    if [ "$(has_changed $cache_file)"  == "false" ]; then
-        log "SUCCESS" "Skipping ${FUNCNAME[0]} because $cache_file is unchanged"
-    else
-        docker build \
-            -t $cache_image \
-            -f docker/npm.docker \
-            .
-        mkdir -p $cache_dir
-        docker run \
-            --rm \
-            -v "$(pwd)"/$cache_dir:/usr/src/app/$cache_dir \
-            $cache_image
-        echo $(sum "$cache_file") > .cache/$cache_file
-        log "SUCCESS" "Finished ${FUNCNAME[0]}"
-    fi
+function colorprint() {
+    # $1 = color number, for example 31, 33, 1;37
+    # $2 = message to be echoed
+    echo -e "\033[$1m$2\033[0m" 
 }
 
 function frontend {
@@ -77,21 +62,16 @@ function wheelhouse {
     log "INFO" "Starting ${FUNCNAME[0]}"
     local image="$NAME"-wheel
     local cache_file=requirements.txt
-    if [ "$(has_changed $cache_file)"  == "false" ]; then
-        log "SUCCESS" "Skipping ${FUNCNAME[0]} because $cache_file is unchanged"
-    else
-        docker build \
-            -t $image \
-            -f docker/wheel.docker \
-            .
-        mkdir -p wheelhouse
-        docker run \
-            --rm \
-            -v "$(pwd)"/wheelhouse:/wheelhouse \
-            $image
-        echo $(sum "$cache_file") > .cache/$cache_file
-        log "SUCCESS" "Finished ${FUNCNAME[0]}"
-    fi
+	docker build \
+		-t $image \
+		-f docker/wheel.docker \
+		.
+	mkdir -p wheelhouse
+	docker run \
+		--rm \
+		-v "$(pwd)"/wheelhouse:/wheelhouse \
+		$image
+	log "SUCCESS" "Finished ${FUNCNAME[0]}"
 }
 
 function app {
@@ -104,7 +84,6 @@ function app {
     log "SUCCESS" "Finished ${FUNCNAME[0]}"
 }
 
-npm_cache
 frontend
 wheelhouse
 app
