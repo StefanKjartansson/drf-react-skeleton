022197b07285b014277f98a4bb2488ab86b08da8
Stefan Kjartansson <esteban.supreme@gmail.com>
2016-11-18 13:15:03 +0000

    Add jsdom

--- frontend/tests/setup.js
+++ frontend/tests/setup.js
@@ -1,1 +1,15 @@
 import 'babel-polyfill';
+
+var jsdom = require('jsdom').jsdom;
+
+global.document = jsdom('');
+global.window = document.defaultView;
+Object.keys(document.defaultView).forEach((property) => {
+  if (typeof global[property] === 'undefined') {
+    global[property] = document.defaultView[property];
+  }
+});
+
+global.navigator = {
+  userAgent: 'node.js'
+};
