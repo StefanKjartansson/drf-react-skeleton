0b7d852383876a24e8edc8caf4eab411b64df999
Stefan Kjartansson <esteban.supreme@gmail.com>
2016-11-18 08:00:35 +0000

    remove hardcoding for postgres

--- project/settings/database.py
+++ project/settings/database.py
@@ -2,21 +2,8 @@
 # -*- coding: utf-8 -*-
 """
 """
-from __future__ import absolute_import, print_function, unicode_literals
-
-import os
 import dj_database_url
 
-try:
-    DATABASES = {
-       'default': {
-           'ENGINE': 'django.db.backends.postgresql_psycopg2',
-           'NAME': 'postgres',                      
-           'USER': 'postgres',
-           'PASSWORD': os.environ['DB_ENV_POSTGRES_PASSWORD'],
-           'HOST': 'db',                      
-        }
-    }
-except:
-    DATABASES = {'default': dj_database_url.config()}
-
+DATABASES = {
+    'default': dj_database_url.config(),
+}
